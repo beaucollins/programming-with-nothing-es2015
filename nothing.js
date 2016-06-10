31b94647b7a2e8ba0c510304a169821c4d5e2341
Robert Collins <beaucollins@gmail.com>
2016-06-10 13:46:54 -0700

    Defer execution of mod

--- nothing.js
+++ nothing.js
@@ -23,7 +23,7 @@ export const IF = b => b
 export const IS_ZERO = n => n( () => FALSE )( TRUE )
 export const IS_LESS_OR_EQUAL = m => n => IS_ZERO( SUBTRACT( m )( n ) )
 
-export const MOD = m => n => IF( IS_LESS_OR_EQUAL( n )( m ) )( MOD( SUBTRACT( m )( n ) )( n ) )( m )
+export const MOD = m => n => IF( IS_LESS_OR_EQUAL( n )( m ) )( x => MOD( SUBTRACT( m )( n ) )( n )( x ) )( m )
 
 export const TWO = p => x => p( p( x ) )
 export const THREE = p => x => p( p( p( x ) ) )
